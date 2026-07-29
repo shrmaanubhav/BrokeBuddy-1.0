a8513b2fceb80309f24c1d5103d46d6e73309e8b
Anubhav Sharma <shrmaanubhav94@gmail.com>
2026-07-29 19:00:18 +0530

    strcuture solidified and code working

--- web-app/src/app/AppRoutes.jsx
+++ web-app/src/app/AppRoutes.jsx
@@ -4,8 +4,6 @@ import ProtectedRoute from "./ProtectedRoute";
 
 import Dashboard from "../pages/Dashboard/Dashboard";
 import ExpensesPage from "../pages/Expenses/Expenses";
-import ProfilePage from "../pages/Profile/Profile";
-import SettingsPage from "../pages/Settings/Settings";
 import Login from "../pages/Landing/Login";
 import ChatBot from "../pages/Chat/Chat";
 
@@ -42,14 +40,7 @@ export default function AppRoutes({
           path="/chatbot"
           element={<ChatBot setIsAuthenticated={setIsAuthenticated} />}
         />
-        <Route
-          path="/profile"
-          element={<ProfilePage setIsAuthenticated={setIsAuthenticated} />}
-        />
-        <Route
-          path="/settings"
-          element={<SettingsPage setIsAuthenticated={setIsAuthenticated} />}
-        />
+
       </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />
