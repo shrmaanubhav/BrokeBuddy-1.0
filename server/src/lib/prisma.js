fe0ecf210144d39c4f92f5fec1e12b87b8a45f0c
Anubhav Sharma <shrmaanubhav94@gmail.com>
2026-08-07 02:16:40 +0530

    cleanup and deployed

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["error"],
 });
 
 export default prisma;
