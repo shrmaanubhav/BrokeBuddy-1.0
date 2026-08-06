cfde042c0392e3f2518bda76cf5e3bdcd3136687
Anubhav Sharma <shrmaanubhav94@gmail.com>
2026-08-07 01:04:15 +0530

    config cors for production

--- server/src/index.js
+++ server/src/index.js
@@ -25,11 +25,8 @@ app.use(passport.initialize());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
-  "http://localhost:3001",
-  "http://127.0.0.1:3001",
-];
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 
 app.use(
   cors({
