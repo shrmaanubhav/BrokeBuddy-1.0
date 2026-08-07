32040259d6d905e01082fbd6724e52e69debe971
Anubhav Sharma <shrmaanubhav94@gmail.com>
2026-08-07 23:07:05 +0530

    fixed cors

--- server/src/index.js
+++ server/src/index.js
@@ -24,6 +24,9 @@ const normalizeOrigin = (origin) => origin?.replace(/\/$/, "");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://brokebuddy.app",
+  "https://www.brokebuddy.app",
+
   process.env.FRONTEND_URL,
 ]
   .filter(Boolean)
